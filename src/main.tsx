dbf18ee63d0335cd19798c67b21ab3d1ec15d786
evozago <evozago@gmail.com>
2025-09-16 12:59:21 -0300

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
 cat > src/main.tsx <<'TSX'
-=======
-<<<<<<< HEAD
->>>>>>> c645543 (chore: ignore .env (não versionar chaves))
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
@@ -13,19 +9,4 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
     <App />
   </React.StrictMode>
 );
-<<<<<<< HEAD
 TSX
-=======
-=======
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
-
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
->>>>>>> bf8c7a6 (chore: ignore .env (não versionar chaves))
->>>>>>> c645543 (chore: ignore .env (não versionar chaves))
