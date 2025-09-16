f08d8e758e51a21340b515d38be0be6672e85d60
evozago <evozago@gmail.com>
2025-09-16 18:23:50 -0300

    Update router.tsx

--- src/router.tsx
+++ src/router.tsx
@@ -12,6 +12,11 @@ import ContasLista from "./pages/financeiro/ContasLista";
 import ContaDetalhe from "./pages/financeiro/ContaDetalhe";
 import ContaNova from "./pages/financeiro/ContaNova";
 
+// RECORRENTES
+import RecorrentesList from "./pages/recorrentes/RecorrentesList";
+import RecorrenteEditar from "./pages/recorrentes/RecorrenteEditar";
+import RecorrentesLog from "./pages/recorrentes/RecorrentesLog";
+
 // guard
 import Protected from "./routes/Protected";
 
@@ -19,7 +24,7 @@ export const router = createBrowserRouter([
   // pública
   { path: "/login", element: <Login /> },
 
-  // redirecionamos a home para ENTIDADES (ou troque para AP se preferir)
+  // home redirecionada para ENTIDADES (pode mudar depois)
   {
     path: "/",
     element: (
@@ -30,54 +35,18 @@ export const router = createBrowserRouter([
   },
 
   // ENTIDADES
-  {
-    path: "/entidades",
-    element: (
-      <Protected>
-        <ListarEntidades />
-      </Protected>
-    ),
-  },
-  {
-    path: "/entidades/nova",
-    element: (
-      <Protected>
-        <EditarEntidade mode="create" />
-      </Protected>
-    ),
-  },
-  {
-    path: "/entidades/:id",
-    element: (
-      <Protected>
-        <EditarEntidade mode="edit" />
-      </Protected>
-    ),
-  },
+  { path: "/entidades", element: <Protected><ListarEntidades /></Protected> },
+  { path: "/entidades/nova", element: <Protected><EditarEntidade mode="create" /></Protected> },
+  { path: "/entidades/:id", element: <Protected><EditarEntidade mode="edit" /></Protected> },
 
   // CONTAS A PAGAR
-  {
-    path: "/financeiro/contas",
-    element: (
-      <Protected>
-        <ContasLista />
-      </Protected>
-    ),
-  },
-  {
-    path: "/financeiro/contas/nova",
-    element: (
-      <Protected>
-        <ContaNova />
-      </Protected>
-    ),
-  },
-  {
-    path: "/financeiro/contas/:id",
-    element: (
-      <Protected>
-        <ContaDetalhe />
-      </Protected>
-    ),
-  },
+  { path: "/financeiro/contas", element: <Protected><ContasLista /></Protected> },
+  { path: "/financeiro/contas/nova", element: <Protected><ContaNova /></Protected> },
+  { path: "/financeiro/contas/:id", element: <Protected><ContaDetalhe /></Protected> },
+
+  // RECORRENTES
+  { path: "/recorrentes", element: <Protected><RecorrentesList /></Protected> },
+  { path: "/recorrentes/nova", element: <Protected><RecorrenteEditar /></Protected> },
+  { path: "/recorrentes/:id", element: <Protected><RecorrenteEditar /></Protected> },
+  { path: "/recorrentes/log", element: <Protected><RecorrentesLog /></Protected> },
 ]);
