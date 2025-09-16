d8439f7a971bc317055045f13ee9423116d18561
evozago <evozago@gmail.com>
2025-09-16 16:52:45 -0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import { RouterProvider } from "react-router-dom";
 import { router } from "./router";
 
+/* apenas injeta o RouterProvider; as páginas cuidam da própria lógica */
 export default function App() {
   return <RouterProvider router={router} />;
 }
+
