1c6211c27a3f412387eeb85d134bb77389bf800b
evozago <evozago@gmail.com>
2025-09-16 13:41:29 -0300

    Replace App component content with NovaEntidade

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,5 @@
+import NovaEntidade from "./pages/entidades/NovaEntidade";
+
 export default function App() {
-  return (
-    <div style={{ padding: 24, fontFamily: "system-ui, -apple-system, Segoe UI, Roboto" }}>
-      <h1>App rodando ✅</h1>
-      <p>Se você está vendo isto, a montagem do React está ok.</p>
-    </div>
-  );
+  return <NovaEntidade />;
 }
