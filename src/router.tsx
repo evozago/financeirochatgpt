d2588d0b127cb1cdef13498229ed8da04d3bed59
evozago <evozago@gmail.com>
2025-09-16 16:52:28 -0300

    Update router.tsx

--- src/router.tsx
+++ src/router.tsx
@@ -4,6 +4,7 @@ import ListarEntidades from "./pages/entidades/ListarEntidades";
 import EditarEntidade from "./pages/entidades/EditarEntidade";
 import Protected from "./routes/Protected";
 
+/* define rotas públicas e protegidas; cada “element” deve ser um elemento React */
 export const router = createBrowserRouter([
   { path: "/login", element: <Login /> },
   {
