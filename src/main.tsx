cf6aabe0d2a2c9271ae8e7d00a0a61f59aabc6ff
evozago <evozago@gmail.com>
2025-09-16 13:13:50 -0300

    Fix formatting and remove unnecessary cat command

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,3 @@
-cat > src/main.tsx <<'TSX'
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
@@ -9,4 +8,3 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
     <App />
   </React.StrictMode>
 );
-TSX
