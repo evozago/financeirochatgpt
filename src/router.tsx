ccf4e1e639c9eafc496e84dfa8156f4ae82f56e6
evozago <evozago@gmail.com>
2025-09-17 07:44:34 -0300

    Update router.tsx

--- src/router.tsx
+++ src/router.tsx
@@ -7,7 +7,7 @@ import Login from "./pages/Login";
 import ListarEntidades from "./pages/entidades/ListarEntidades";
 import EditarEntidade from "./pages/entidades/EditarEntidade";
 
-// AP (Contas a Pagar)
+// AP
 import ContasLista from "./pages/financeiro/ContasLista";
 import ContaDetalhe from "./pages/financeiro/ContaDetalhe";
 import ContaNova from "./pages/financeiro/ContaNova";
@@ -17,29 +17,24 @@ import RecorrentesList from "./pages/recorrentes/RecorrentesList";
 import RecorrenteEditar from "./pages/recorrentes/RecorrenteEditar";
 import RecorrentesLog from "./pages/recorrentes/RecorrentesLog";
 
+// NFE
+import ImportarNFe from "./pages/nfe/ImportarNFe";
+import ConciliarNFe from "./pages/nfe/ConciliarNFe";
+
 // guard
 import Protected from "./routes/Protected";
 
 export const router = createBrowserRouter([
-  // pública
   { path: "/login", element: <Login /> },
 
-  // home redirecionada para ENTIDADES (pode mudar depois)
-  {
-    path: "/",
-    element: (
-      <Protected>
-        <ListarEntidades />
-      </Protected>
-    ),
-  },
+  { path: "/", element: <Protected><ListarEntidades /></Protected> },
 
   // ENTIDADES
   { path: "/entidades", element: <Protected><ListarEntidades /></Protected> },
   { path: "/entidades/nova", element: <Protected><EditarEntidade mode="create" /></Protected> },
   { path: "/entidades/:id", element: <Protected><EditarEntidade mode="edit" /></Protected> },
 
-  // CONTAS A PAGAR
+  // AP
   { path: "/financeiro/contas", element: <Protected><ContasLista /></Protected> },
   { path: "/financeiro/contas/nova", element: <Protected><ContaNova /></Protected> },
   { path: "/financeiro/contas/:id", element: <Protected><ContaDetalhe /></Protected> },
@@ -49,4 +44,8 @@ export const router = createBrowserRouter([
   { path: "/recorrentes/nova", element: <Protected><RecorrenteEditar /></Protected> },
   { path: "/recorrentes/:id", element: <Protected><RecorrenteEditar /></Protected> },
   { path: "/recorrentes/log", element: <Protected><RecorrentesLog /></Protected> },
+
+  // NFe
+  { path: "/nfe/importar", element: <Protected><ImportarNFe /></Protected> },
+  { path: "/nfe/conciliar", element: <Protected><ConciliarNFe /></Protected> },
 ]);
